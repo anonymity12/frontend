8c2e5bd0a481d9439411cf49826e9c248e85bf81
Your Name <you@example.com>
2022-01-05 14:28:04 +0800

    vbind属性绑定

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <label for="r1">修改颜色</label><input type="checkbox" v-model="use" id="r1">
+    <br><br>
+    <div v-bind:class="{'class1': use}">
+      v-bind:class 指令
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      use: 'false'
+    }
+  }
 }
 </script>
 
@@ -19,5 +27,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.class1{
+  background: #444;
+  color: #eee;
 }
 </style>
