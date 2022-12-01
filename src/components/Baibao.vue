0f910397de1468fed765113531e71d65f0abcad6
ttAtTencentCloud <1135175824@qq.com>
2022-12-01 17:54:59 +0800

    fix dialog width to 100

--- src/components/Baibao.vue
+++ src/components/Baibao.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <el-dialog title="百宝箱" :visible.sync="boxShow.show">
+        <el-dialog title="百宝箱" :visible.sync="boxShow.show" width="100%">
                 <el-table :data="gridData" height="290" >
                     <el-table-column property="date" label="日期" width="150"></el-table-column>
                     <el-table-column property="name" label="姓名" width="200"></el-table-column>
