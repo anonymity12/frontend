529f29439d9d43fc0b423e5aa073bb89cf6a0eb1
ttAtTencentCloud <1135175824@qq.com>
2023-01-22 21:29:01 +0800

    better href link

--- src/components/SixLog.vue
+++ src/components/SixLog.vue
@@ -7,7 +7,7 @@
       active-text-color="red"
       >
       <el-menu-item>
-        <a href="http://101.43.166.211:8091/#/home">返回主站</a>
+        <a href="/home">返回主站</a>
       </el-menu-item>
       <span style="position: absolute;padding-top: 20px;right: 43%;font-size: 20px;font-weight: bold">
         浮生六记
