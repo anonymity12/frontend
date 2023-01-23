11c2fcc4d20f8c258024c98b11771f382dbc72d5
ttAtTencentCloud <1135175824@qq.com>
2023-01-23 19:28:43 +0800

    update ui trivial

--- src/components/ImgUpload.vue
+++ src/components/ImgUpload.vue
@@ -14,7 +14,7 @@
     :on-exceed="handleExceed"
     :file-list="fileList">
     <el-button size="small" type="primary">点击上传</el-button>
-    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+    <div slot="tip" class="el-upload__tip">只能上传一张jpg/png文件，且不超过500kb</div>
   </el-upload>
 </template>
 
