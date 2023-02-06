3c3710102f966c30945110674f91ee05c7ba845f
anonymity12 <ztt1995120@gmail.com>
2023-02-06 15:49:56 +0800

    more funcs life indi

--- src/components/LifeIndicator.vue
+++ src/components/LifeIndicator.vue
@@ -1,28 +1,51 @@
 <template>
   <div style="text-align: left">
-    <h3>{{ msg }}</h3>
+    <span class="profile-span-text">{{ msg }}</span>
     <ul>
-    <li> 🔺{{ this.$store.state.user.cname }} 已有 &nbsp;<strong>{{ lifeIndicator.dayPassed }}</strong>  &nbsp;天使用</li>
-    <li> 🔻{{ this.$store.state.user.cname }}  还有 <strong>{{ dayLeft }}</strong> 天可用 </li>
+    <li class="profile-span-text"> 🔺{{ this.$store.state.user.cname }} 已有 &nbsp;<strong>{{ lifeIndicator.dayPassed }}</strong>  &nbsp;天使用</li>
+    <li class="profile-span-text"> 🔻{{ this.$store.state.user.cname }}  还有 <strong>{{ dayLeft }}</strong> 天可用 </li>
     </ul>
     <el-progress :text-inside="true" :stroke-width="26" :percentage="calculatePerc()"></el-progress>
-    <a>生命进度条&不断更新的乖娃娃应用</a>
-    <span style="float: right;">
-      <audio controls autoplay muted>
-        <source src="http://101.43.166.211/2awa.mp3" type="audio/mpeg">
-      Your browser does not support the audio element.
-      </audio>
-    </span>
+    <!-- align for verticle, justify for horizontal -->
+    <el-row style="margin: 8px;" type="flex" justify="space-between" align="middle">
+      <span class="profile-span-text">生命进度条&不断更新的乖娃娃应用</span>
+      <span>
+        <audio controls loop="loop" id="bg-music" style="display: none; height: 0dp">
+          <source src="http://101.43.166.211/2awa.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+        </audio>
+      </span>
+      <span class="profile-span-text">当前音乐清单为：<el-tag effect="plain" size="mini">2awa.mp3</el-tag>, 由 <el-tag size="mini">通通</el-tag> 创建</span>
+      
+    </el-row>
+    
+    <div class="function-area">
+      <el-button type="primary" round @click="handleClick(1)" class="profile-span-text">
+        浮生六记
+      </el-button>
+      <el-button type="primary" round @click="handleClick(2)" class="profile-span-text">
+        {{ this.musicState? '暂停音乐':'播放音乐' }}
+      </el-button>
+      <el-button type="primary" round @click="handleClick(3)" class="profile-span-text">
+        查看百宝箱
+      </el-button>
+    </div>
+    <Baibao :boxShow="boxShow"></Baibao>
 
   </div>
 </template>
 
 <script>
 import { getLifeIndicator } from '@/api/user'
+import Baibao from '@/components/Baibao'
+
 export default {
-  name: 'LogDetail',
+  name: 'LifeIndicator',
   data () {
     return {
+      boxShow: {
+        show: false
+      },
       msg: '生命指示器 ⏳',
       // who: '天天', todo: remove all comment `who` later 0201 12:17
     //   calcPercent: 1,
@@ -32,7 +55,8 @@ export default {
         dayPassed: 1,
         dayAll: 33000,
         userId: 1
-      }
+      },
+      musicState: false
     }
   },
   methods: {
@@ -53,10 +77,38 @@ export default {
             }
         })
         
+    },
+    handleClick(pos) {
+      switch(pos) {
+        case 1:
+          this.$router.push({path: '/sixlog'})
+          break 
+        case 2:
+          var audio = document.getElementById("bg-music")
+
+          if (this.musicState == false) {
+            audio.play()
+            this.musicState = true
+          } else {
+            audio.pause()
+            this.musicState = false
+          }
+          break 
+        case 3:
+          this.boxShow.show = true 
+          break
+        default:
+          break 
+
+      }
+       
     }
   },
   created() {
     this.callGetLifeIndicator()
+  },
+  components: {
+    Baibao
   }
 }
 </script>
@@ -79,4 +131,9 @@ li {
 a {
   color: #42b983;
 }
+
+.profile-span-text{
+  font-family: cursive;
+}
+
 </style>
