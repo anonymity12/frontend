cae4b373efb71fcf041de2607e1f5f17845c321e
anonymity12 <ztt1995120@gmail.com>
2023-10-28 16:13:15 +0800

    task: change input box position

--- src/components/TaskMatrix.vue
+++ src/components/TaskMatrix.vue
@@ -1,22 +1,5 @@
 <template>
     <div>
-        <el-card class="box-card">
-            <div slot="header" class="clearfix">
-                <span style="float: left;font-size: large;
-                    font-weight: bold;">
-                    任务面板
-                </span>
-            </div>
-            <el-row type="flex" style="width: 100%;" class="row-container">
-                <el-select v-model="matrixSelection" placeholder="请选择任务象限">
-                    <el-option v-for="item in matrixOptions" :key="item.value" :label="item.label" :value="item.value">
-                    </el-option>
-                </el-select>
-                <textarea cols="50" rows="5" @keyup.enter="addTask"
-                    style="text-shadow: gray 4px 4px 6px; font-size: large; margin-bottom: 15px;" v-model="newTaskTitle"
-                    placeholder="不断的小成功，是大成功的催化剂。有什么事要搞定？"></textarea>
-            </el-row>
-        </el-card>
         <!-- 第一行，两列 -->
         <el-row>
             <el-col :span="12">
@@ -106,6 +89,23 @@
                 </div>
             </el-col>
         </el-row>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <span style="float: left;font-size: large;
+                    font-weight: bold;">
+                    任务面板
+                </span>
+            </div>
+            <el-row type="flex" style="width: 100%;" class="row-container">
+                <el-select v-model="matrixSelection" placeholder="请选择任务象限">
+                    <el-option v-for="item in matrixOptions" :key="item.value" :label="item.label" :value="item.value">
+                    </el-option>
+                </el-select>
+                <textarea cols="50" rows="5" @keyup.enter="addTask"
+                    style="text-shadow: gray 4px 4px 6px; font-size: large; margin-bottom: 15px;" v-model="newTaskTitle"
+                    placeholder="不断的小成功，是大成功的催化剂。有什么事要搞定？"></textarea>
+            </el-row>
+        </el-card>
     </div>
 </template>
 <script>
