ba84e832533f4e9ecabbf9975c2f3eadcd940093
ttAtTencentCloud <1135175824@qq.com>
2024-01-21 16:00:29 +0800

    中间的文本无错误旋转了

--- src/components/JiaPuSunburst.vue
+++ src/components/JiaPuSunburst.vue
@@ -293,6 +293,10 @@ export default {
             }
 
             function labelTransform(d) {
+                console.log("d is : ", d)
+                if (d.depth == 0) {
+                    return `rotate(0) translate(0,0)`
+                }
                 const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
                 const y = (d.y0 + d.y1) / 2 * radius;
                 return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
