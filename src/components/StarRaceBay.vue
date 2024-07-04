7e554ea36555cdd4f74aae49c2ac9b0fde95305b
ttAtTencentCloud <1135175824@qq.com>
2024-07-04 09:31:05 +0800

    fix id=15 user, gg userface didnt show issue

--- src/components/StarRaceBay.vue
+++ src/components/StarRaceBay.vue
@@ -2,12 +2,12 @@
     <div id="statistic-container">
     <span><b>🏅️本周排行榜🏅️</b></span>
         <div class="all-statistic" v-for="item in userAndTheirStarCount" :key="item.userId">
-            <div style="display: flex; align-items: center;" @click="openWeeklySummaryPage()">
+            <div style="display: flex; justify-content: right;align-items: center;" @click="openWeeklySummaryPage()">
                 <el-avatar 
-                    style="color: #222; margin: 20px;"
+                    style="color: #fff; margin: 20px;"
                     :src="item.userface"
-                    :size="50"
-                    >
+                    :size="70"
+                    :fit="'cover'">
                 </el-avatar>
                 <span>{{ item.username }}🔮</span>
                 <span><b>{{ item.cnt }}</b></span>
@@ -27,13 +27,13 @@ export default {
         return {
             userAndTheirStarCount: [
                 {
-                    userId: 15,
+                    userId: 0,
                     username: 'tt1',
                     userface: '../assets/starSmile2.jpg',
                     cnt: 3
                 },
                 {
-                    userId: 17,
+                    userId: 1,
                     username: 'tt4',
                     userface: '../assets/starSmile.jpg',
                     cnt: 7
